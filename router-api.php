554dbf205e525ca8b130384f7e01cd2dfb211852
Manuel Dualde <manueldualde2012@gmail.com>
2024-11-09 17:33:13 -0300

    se agrega ruteo para mostrar listado de comentarios

--- router-api.php
+++ router-api.php
@@ -7,6 +7,9 @@ $router = new Router();
 
 // define la tabla de ruteo
 #                 endpoint         verbo        controller                     método
+$router->addRoute('canciones/:id/comentarios',        'GET',       'ApiController',         'getAllByCancion');
+$router->addRoute('comentarios',                      'GET',       'ApiController',         'getAll');
+$router->addRoute('comentarios/:id',                  'PUT',       'ApiController',         'update');
 
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
